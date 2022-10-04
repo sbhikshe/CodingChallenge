47f579e5998ecde8846d0379071bee5baf30cb27
Priya B <priya_saran@yahoo.com>
2022-10-03 20:57:44 -0700

    clarified scenarios of displaying questions, stop timer if timer goes negative (not just 0)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+/* Intro to the quiz, start button, timer display */
 var startButtonEl = document.querySelector("#startButton");
 var introEl = document.querySelector("#intro");
 var timerEl = document.querySelector("#time-remaining");
@@ -24,7 +25,9 @@ var highScoresListOl;
 var highScoresLi = []; /* append initials + score and display as a single string li */
 var goBackButtonEl;
 var clearHighScoresButtonEl;
+var score = 0; 
 
+/* Questions, answer choices, correct answer */
 var quizTime = 100;
 var qa1 = {
     question: "The header tag appears in which portion of the HTML document?",
@@ -44,110 +47,124 @@ var qa3 = {
     answer: "any combination of types"
 };
 
-
 var qaList = [qa1, qa2, qa3];                
 var showNext = true;
 var currentQuestion;
 var nextQuestion = 0;
+var skipToNext = true; /* did the user skip to the next question without picking an answer */
 
-var score = 0; 
+function runQuiz(event) {
 
-function displayQuestion(event) {
-
-    /* If we came to this on the next button click, 
-    we want to set the showNext to true to display 
-    the next question. Else we don't know that the user 
-    already responded and we've show correct / wrong. */
-    if(event && event.target === nextButtonEl) {
-        showNext = true;
-    }
-    /* we are done, no more Qs to show */
-    if (nextQuestion >= qaList.length) {
-        if(showNext === false) {
-            /* we are waiting for the user to answer the last question */
-            console.log("No more questions. Waiting for the last answer");
-            return "same";
+    /* we use the following conditions to run this quiz / display questions */
+    /* nextquestion - index of the next question to be displayed */
+    /*              - used with qalist.length to check for complete */
+    /*              if 0, we need to display the first question, so set up the DOM */
+    /*              if > 0 but < max, check showNext */
+    /*              if == max, check showNext */
+    /*              if > max, done */
+    /* showNext     - if false, we are waiting for the user to respond */
+    /*              - if true, we are ready to show the next question */
+    
+    if (nextQuestion < qaList.length) {
+        if(showNext == true) {
+            /* setup DOM if required, and display the next question */
+            displayQuestion();
+            showNext = false;
+            return "new";
         } else {
-            /* user has answered the last question, and there are no more */
-            console.log("No more questions to show");
+            console.log("Waiting for user to respond");
+            return "same";
+        }
+    } else if (nextQuestion == qaList.length) {
+        if (showNext == true) {
+            /* shown all the questions, user has responded */
+            console.log("No more to show, return none");
             return "none";
+        } else {
+            console.log("waiting for user to respond to last question");
+            return "same"
         }
-    } else 
-    if (showNext === true) {
-        /* this is the first question or the user answered the current question */
-        /* get the next qa object from the qaList */
-        currentQuestion = qaList[nextQuestion];
+    }
+}
 
-        /* if this is the first question, create the ul/li elements to */
-        /* to show the question and answers */
-        if(nextQuestion === 0) {
+function displayQuestion() {
+    /* get the first or next qa object from the qaList */
+    currentQuestion = qaList[nextQuestion];
 
-            questionEl = document.createElement("p");
-            qaEl.appendChild(questionEl);
+    /* if this is the first question, create the ul/li elements */
+    /* to show the question and answers */
+    /* else elements are already set up, skip this. */
+    if(nextQuestion === 0) {
+        setupQaDisplay();
+    }
 
-            answerListUl = document.createElement("ol");
-            qaEl.appendChild(answerListUl);
+    /* set the question and choices to the ul and li */
+    questionEl.textContent = currentQuestion.question;
+    for (var i = 0; i < 4; i++) {
+        answersLi[i].textContent = currentQuestion.choices[i];
+    }
+    responseToUserEl.textContent = "";
+    answerListUl.addEventListener("click", checkAnswer);
+}
 
-            for (var i = 0; i < 4; i++) {
-                answersLi[i] = document.createElement("li");
-                answerListUl.appendChild(answersLi[i]);
-                answersLi[i].setAttribute("style", "background-color: lightblue; width: 200px; padding: 10px; margin: 5px;");
-                answersLi[i].addEventListener("mouseenter", function(event) {
-                    var item = event.target;
-                    item.setAttribute("style", "background-color: lightgrey; width: 200px; padding: 10px; margin: 5px;");
+function setupQaDisplay() {
+    /* for the question */
+    questionEl = document.createElement("p");
+    qaEl.appendChild(questionEl);
 
-                });
-                answersLi[i].addEventListener("mouseleave", function(event) {
-                    var item = event.target;
-                    item.setAttribute("style", "background-color: lightblue; width: 200px; padding: 10px; margin: 5px;");
+    /* The list of choices for the answer */
+    answerListUl = document.createElement("ol");
+    qaEl.appendChild(answerListUl);
 
-                });
-            }
-
-            /* add the next button */
-            nextButtonEl = document.createElement("button");
-            nextButtonEl.textContent = "Next";
-            nextButtonEl.setAttribute("style", "padding: 5px; margin: 10px;")
-            qaEl.appendChild(nextButtonEl);
-
-            responseToUserEl = document.createElement("p");
-            responseToUserEl.textContent = "";
-            responseToUserEl.setAttribute("style", "font-size: 150%; padding: 5px; margin-left: 20px;")
-            qaEl.appendChild(responseToUserEl);
-        }
-
-        /* set the question and choices to the ul and li */
-        questionEl.textContent = currentQuestion.question;
-        for (var i = 0; i < 4; i++) {
-            answersLi[i].textContent = currentQuestion.choices[i];
-        }
-        responseToUserEl.textContent = "";
-        answerListUl.addEventListener("click", checkAnswer);
-        /*
-        answerListUl.addEventListener("mouseenter", function(event) {
+    for (var i = 0; i < 4; i++) {
+        answersLi[i] = document.createElement("li");
+        answerListUl.appendChild(answersLi[i]);
+        answersLi[i].setAttribute("style", "background-color: lightblue; width: 200px; padding: 10px; margin: 5px;");
+        answersLi[i].addEventListener("mouseenter", function(event) {
             var item = event.target;
-            item.setAttribute("style", "background-color: darkblue; ")
-        });
-        answerListUl.addEventListener("mouseleave", function(event) {
-            var item = event.target;
-            item.setAttribute("style", "background-color: lightblue; ")
-        });
-        */
+            item.setAttribute("style", "background-color: lightgrey; width: 200px; padding: 10px; margin: 5px;");
 
-        /* don't show the next until the user has answered this one*/
-        /* showNext should be set to true in the Next button event listener */
-        showNext = false; 
-        /* increment nextQuestion to use the next time */
-        nextQuestion++;
-        return "new"; /* we have put out the next question */
+        });
+        answersLi[i].addEventListener("mouseleave", function(event) {
+            var item = event.target;
+            item.setAttribute("style", "background-color: lightblue; width: 200px; padding: 10px; margin: 5px;");
+
+        });
+    }
+
+    /* add the next button */
+    nextButtonEl = document.createElement("button");
+    nextButtonEl.textContent = "Next";
+    nextButtonEl.setAttribute("style", "padding: 5px; margin: 10px;")
+    qaEl.appendChild(nextButtonEl);
+    nextButtonEl.addEventListener("click", handleNextButton);
+
+    /* add this element to display correct / wrong when the answer is checked */
+    responseToUserEl = document.createElement("p");
+    responseToUserEl.textContent = "";
+    responseToUserEl.setAttribute("style", "font-size: 150%; padding: 5px; margin-left: 20px;")
+    qaEl.appendChild(responseToUserEl);
+}
+
+function handleNextButton() {
+    if(skipToNext === true) {
+        /* user didn't answer the question, skipping to the next question */
+        /* negative points! */
+        console.log("user didn't answer the question, negative points");
+        quizTime -= 10;
     } else {
-        console.log("showNext is not true, waiting for the user to respond or click next");
-        return "same";
-    }   
+        console.log("user answered this Q, reset skipToNext");
+        skipToNext = true;
+    }
+    /* user clicked next, show the next question */
+    showNext = true;
+    nextQuestion++;
 }
 
 function checkAnswer(event) {
     console.log("In check answer: ", event);
+    /* user clicked on a choice, didn't skip */
+    skipToNext = false; 
     /* check the answer */
     /* show correct / wrong */
     console.log("event.target" + event.target);
@@ -169,14 +186,12 @@ function checkAnswer(event) {
 }
 
 function displayScores() {
-    console.log("Need to show the score");
     /* clear the page, remove all the qa elements */
     qaEl.removeChild(questionEl);
     qaEl.removeChild(answerListUl);
     for (var i = 0; i < 4; i++) {
         answerListUl.removeChild(answersLi[i]);
     }
-
     qaEl.removeChild(nextButtonEl);
     qaEl.removeChild(responseToUserEl);
 
@@ -191,9 +206,7 @@ function displayScores() {
     scoreEl.setAttribute("style", "font-weight: bold; padding: 5px; margin: 10px;");
     qaEl.appendChild(scoreEl);  
 
-    /* Enter initials + Submit */
     /* This <div> contains "Enter Initials: <input box> Submit button " */
-
     initialsDivEl = document.createElement("div");
     qaEl.appendChild(initialsDivEl);
 
@@ -213,7 +226,7 @@ function displayScores() {
     submitButtonEl.setAttribute("style", "padding: 5px; margin-left: 10px;");
     initialsDivEl.appendChild(submitButtonEl);
 
-    /* when you hit submit, stores to local storage */
+    /* when the user clicks Submit, store to local storage */
     submitButtonEl.addEventListener("click", handleScoreSubmit);
     
 }
@@ -240,16 +253,19 @@ function handleScoreSubmit() {
     highScoresListOl = document.createElement("ol");
     qaEl.appendChild(highScoresListOl);
 
+    /* for now, storing only one score */
     highScoresLi[0] = document.createElement("li");
     highScoresLi[0].textContent = localStorage.getItem("score");
     highScoresListOl.appendChild(highScoresLi[0]);
 
+    /* add the Go Back button */
     goBackButtonEl = document.createElement("button");
     goBackButtonEl.textContent = "Go Back";
     goBackButtonEl.setAttribute("style", "margin-left: 10px; margin-bottom: 10px");
     goBackButtonEl.addEventListener('click', goBackToStart);
     qaEl.appendChild(goBackButtonEl);
 
+    /* add the Clear High Scores button */
     clearHighScoresButtonEl = document.createElement("button");
     clearHighScoresButtonEl.textContent = "Clear High scores";
     clearHighScoresButtonEl.setAttribute("style", "margin-left: 20px; margin-bottom: 10px");
@@ -271,29 +287,17 @@ function goBackToStart() {
     qaEl.removeChild(clearHighScoresButtonEl);
 
     /* return to start */
-    resetTime();
-    /*
-    introEl.children[0].textContent = "This is a timed multiple choice quiz with only one correct answer for each question. Good luck!";
-    startButtonEl.disabled = false;
-    */
+    quizTime = 100;
+    timerEl.textContent = quizTime;
     introEl.setAttribute("style", "visibility: visible;");
     startButtonEl.setAttribute("style", "visibility: visible;");
     showNext = true;
     nextQuestion = 0;
 }
 
-function resetTime() {
-    quizTime = 100;
-    timerEl.textContent = quizTime;
-}
-
 function startQuiz() {
 
-    /* Remove intro to the game, and make Start button inactive */
-    /*
-    introEl.children[0].textContent = "";
-    startButtonEl.disabled = true;
-    */
+    /* Hide the intro to the game and the Start button */
     introEl.setAttribute("style", "visibility: hidden;");
     startButtonEl.setAttribute("style", "visibility: hidden;");
 
@@ -301,19 +305,18 @@ function startQuiz() {
         quizTime--;
         timerEl.textContent = quizTime;
 
-        /* if out of time, stop timer */
-        /* and show scores */
-        /* else */
-        /* display questions */
-        if (quizTime === 0) {
+        /* if out of time, stop timer and show the score */
+        /* else, display the next questions */
+        /* quizTime can also be < 0, since the user loses points (-10) */
+        /* if they skip a question without answering */
+        if (quizTime <= 0) {
             clearInterval(timerId);
             displayScores();
         } else {
-            var isQuestionDisplayed = displayQuestion();
+            var isQuestionDisplayed = runQuiz();
             if ( isQuestionDisplayed === "new") {
                 /* add the next button listener */
                 console.log("new question displayed, wait for answer");
-                nextButtonEl.addEventListener("click", displayQuestion);
             } else if (isQuestionDisplayed === "same") {
                 console.log("Continue to display same question, Waiting for user to respond");
             } else if (isQuestionDisplayed === "none") {
@@ -328,4 +331,3 @@ function startQuiz() {
 }
 
 startButtonEl.addEventListener("click", startQuiz);
-
